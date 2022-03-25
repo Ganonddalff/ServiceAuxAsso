3bba0e8cbd38ee653ec425199ee8e16efd7988cb
Paul-Kammegne <kammegne2002@yahoo.fr>
2022-03-25 11:51:51 +0100

    Commit Crowfunding

--- AssoFlex/Controllers/CrowdfundingController.cs
+++ AssoFlex/Controllers/CrowdfundingController.cs
@@ -1,14 +1,22 @@
+using AssoFlex.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace AssoFlex.Controllers
 {
     public class CrowdfundingController : Controller
     {
+        private IDal _dal;
+
+        public CrowdfundingController()
+        {
+            this._dal = new Dal();
+        }
         // GET
         public IActionResult Index()
         {
-            
-            return View();
+            List<Crowdfunding> listeDesCrowdfundings = _dal.getAllCrowdfundings();
+            return View(listeDesCrowdfundings);
         }
     }
 }
