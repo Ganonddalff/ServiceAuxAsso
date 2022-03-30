fa82cda0a0cd58c7bc18153dd8e07170c8a0d0c3
Ganonddalff <m-ateusz@live.fr>
2022-03-30 11:24:09 +0200

    Ajout models interface et layout pour widget

---
+++ AssoFlex/Models/IWidget.cs
@@ -0,0 +1,11 @@
+namespace AssoFlex.Models
+{
+    public interface IWidget
+    {
+        int SortOrder { get; set; }
+        string ClassName { get; set; }
+        string FooterText { get; set; }
+        string HeaderText { get; set; }
+        ISubWidget SubWidget { get; set; }
+    }
+}
---
+++ AssoFlex/Models/NoticeBoard.cs
@@ -0,0 +1,11 @@
+namespace AssoFlex.Models
+{
+    public class NoticeBoard : IWidget
+    {
+        public int SortOrder { get; set; }
+        public string ClassName { get; set; }
+        public string FooterText { get; set; }
+        public string HeaderText { get; set; }
+        public ISubWidget SubWidget { get; set; }
+    }
+}
