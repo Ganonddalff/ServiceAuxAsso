cba73403dc4f08f3a556f46bcc83dcaa4494f904
Piercuta <pierre.courteille@gmail.com>
2022-03-31 15:32:25 +0200

    update file for aws deploy

--- AssoFlex/Program.cs
+++ AssoFlex/Program.cs
@@ -18,6 +18,18 @@ namespace AssoFlex
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    if (System.Diagnostics.Debugger.IsAttached)
+                    {
+                        Console.WriteLine("Debug mode");
+                        webBuilder.UseStartup<Startup>();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Release mode");
+                        webBuilder.UseStartup<Startup>().UseUrls("http://0.0.0.0:5000", "http://0.0.0.0:5001");
+                    }
+                });
     }
 }
