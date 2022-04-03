f45fa2a1cb8e8a9737115adaf7cd81a1a04d725d
gkiori <gkastane@gmail.com>
2022-04-03 14:41:59 +0200

    Mise en page de la page accueil

--- AssoFlex/wwwroot/js/site.js
+++ AssoFlex/wwwroot/js/site.js
@@ -2,3 +2,38 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+jQuery(
+    function($) {
+        $(window).on('scroll', function() {
+            if ($(this).scrollTop() <= 50) {
+                $('.navbar').addClass('fixed-top');
+            } 
+            // else if ($(this).scrollTop() === 0) { //
+            //     $('.navbar').removeClass('fixed-top');
+            // }
+    });
+
+    function adjustNav() {
+        var winWidth = $(window).width(),
+            dropdown = $('.dropdown'),
+            dropdownMenu = $('.dropdown-menu');
+
+        if (winWidth >= 768) {
+            dropdown.on('mouseenter', function() {
+                $(this).addClass('show')
+                    .children(dropdownMenu).addClass('show');
+            });
+
+            dropdown.on('mouseleave', function() {
+                $(this).removeClass('show')
+                    .children(dropdownMenu).removeClass('show');
+            });
+        } else {
+            dropdown.off('mouseenter mouseleave');
+        }
+    }
+
+    $(window).on('resize', adjustNav);
+
+    adjustNav();
+});
