6c2ff4d7cd582b7fc3b76f1e359ffbaf4767c5e8
gkiori <gkastane@gmail.com>
2022-04-05 08:31:37 +0200

    times is out

--- AssoFlex/Controllers/PanierController.cs
+++ AssoFlex/Controllers/PanierController.cs
@@ -34,9 +34,29 @@ namespace AssoFlex.Controllers
         public IActionResult DeleteArticle(int idUser, int idArticle)
         {
             Panier panier = _dal.GetPanierByUserId(idUser);
-            // List<ArticlePanier> articlePaniers = _dal.GetArticlesPanierByPanierId(panier.Id);
             _dal.DeleteArticlePanier(idArticle, panier.Id);
             return RedirectToAction("Index", "Panier", new {idUser=User.FindFirstValue(ClaimTypes.NameIdentifier)});
+        }
+
+        [HttpPost]
+        public IActionResult Payer(LayoutModelView layoutModelView)
+        {
+            for (int i = 0; i < layoutModelView.Panier.ArticlesPanier.Count; i++)
+            {
+                if (layoutModelView.Panier.ArticlesPanier[i].TypeDeCommande == "Adhesion")
+                {
+                    
+                }
+                if (layoutModelView.Panier.ArticlesPanier[i].TypeDeCommande == "Evenement")
+                {
+                    
+                }
+                if (layoutModelView.Panier.ArticlesPanier[i].TypeDeCommande == "Crowdfunding")
+                {
+                    
+                }
+            }
+            return Ok();
         }
     }
 }
